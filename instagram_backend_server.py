834ae53aeefbfccfada2c62e1b182e9097154458
host207 <iqa20753@gmail.com>
2026-03-06 16:57:36 +0300

    Update instagram_backend_server.py

--- instagram_backend_server.py
+++ instagram_backend_server.py
@@ -113,7 +113,7 @@ def home():
         }
     })
 
-if name == 'main':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     
     logger.info("🚀 Starting DownTik Backend Server...")
